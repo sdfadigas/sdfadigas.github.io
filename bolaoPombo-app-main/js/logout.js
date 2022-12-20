7b838bed7c316c05a3ded33c6934d333bc629038
Samara Fadigas <99447521+sdfadigas@users.noreply.github.com>
2022-12-20 14:11:50 -0300

    Delete bolaoPombo-app-main directory

--- bolaoPombo-app-main/js/logout.js
+++
@@ -1,9 +0,0 @@
-//LOGOUT
-logoutBtn.addEventListener('click', function logout() {
-    firebase.auth().signOut().then(function () {
-        localStorage.setItem("emailUser", undefined)
-        window.location.href = "login.html";
-    }).catch(function (error) {
-        console.error(error);
-    });
-}) 

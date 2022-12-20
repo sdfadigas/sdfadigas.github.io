aca1107d1643001d1d55ff6ef7eed0c6372bf715
Samara Fadigas <99447521+sdfadigas@users.noreply.github.com>
2022-12-20 19:07:06 -0300

    Update loginGoogle.js

--- bolaoPombo-main/js/loginGoogle.js
+++ bolaoPombo-main/js/loginGoogle.js
@@ -1,6 +1,7 @@
 //LOGIN GOOGLE
 const auth = firebase.auth();
 const provider = new firebase.auth.GoogleAuthProvider();
+const welcome = getElementbyId('welcome');
 var authGoogleButton = document.getElementById('authGoogleButton');
 authGoogleButton.addEventListener('click', function () {
     auth.signInWithPopup(provider);
@@ -10,6 +11,7 @@ auth.onAuthStateChanged((val) => {
         console.log(auth.currentUser)
         localStorage.setItem("emailUser", auth.currentUser.email)
         window.location.href="welcome.html"
+        welcome.innerText = ("Seja bem vinda(o)" + auth.currentUser.email)
     }
 });
 
